4e30935432d749a7a082eba309d24a23604d859e
Abdellahsyani <syaniabdos@gmail.com>
2024-09-17 19:21:27 +0100

    ten queen

--- 13_37/c05/ex08/ft_ten_queens_puzzle.c
+++ 13_37/c05/ex08/ft_ten_queens_puzzle.c
@@ -1,84 +1,74 @@
 #include <unistd.h>
 
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-int main()
+int is_safe(int **matrix, int col, int row, int n)
 {
-    int N;
-    int *arr;
-    scanf("%d", &N);
-    arr = malloc(sizeof(int) * N);
-    for (int i = 0; i < N; i++) {
-        int pi;
-        scanf("%d", &pi);
-	arr[i] = pi;
-    }
-    printf("the array before sort\n");
-    for (int u = 0; u < N; u++)
-    {
-            printf("%d", arr[u]);
-    }
-    printf("\n");
-    int i, j;
-    int tem;
-    i = 0;
-    while (i < N - 1)
-    {
-	    j = 0;
-	    while (j < N - 1)
-	    {
-		    if (arr[j] > arr[j+1])
-		    {
-			    tem = arr[j];
-			    arr[j] = arr[j+1];
-			    arr[j+1] = tem;
-		    }
-		    j++;
-	    }
-	    i++;
-    }
-    printf("the array after sort\n");
+	int i;
+	int j;
 
-    for (int u = 0; u < N; u++)
-    {
-	    printf("%d", arr[u]);
-    }
-    printf("\n");
-    int k;
-    int *sort;
-    sort = malloc(sizeof(int) * (N-1));
-    k = 0;
-    while (k < N)
-    {
-	    sort[k] = arr[k+1] - arr[k];
-	    k++;
-    }
-    i = 0;
-    while (i < N - 2)
-    {
-	    j = 0;
-	    while (j < N - 2)
-	    {
-		    if (sort[j] > sort[j+1])
-		    {
-			    tem = sort[j];
-			    sort[j] = sort[j+1];
-			    sort[j+1] = tem;
-		    }
-		    j++;
-	    }
-	    i++;
-    }
-    printf("%d\n", sort[0]);
+	i = 0;
+	while (i < n)
+	{
+		if (matrix[i][col] == 1)
+			return 0;
+		i++;
+	}
+	i = row;
+	j = col;
+	while (i >= 0 && j >= 0)
+	{
+		if (matrix[i][j] == 1)
+			return 0;
+		i--;
+		j--;
+	}
+	i = row;
+	j = col;
+	while (i < n && j >= 0)
+	{
+		if (matrix[i][j] == 1)
+			return 0;
+		i++;
+		j--;
+	}
+	return (1);
+}
 
-    return 0;
+int handle_chess(int **matrix, int col, int row, int n)
+{
+	if (is_safe(**matrix, col, row, n))
+	{
+
+}
+
+void chess_board(int **matrix, int n)
+{
+	int i;
+	int col;
+	int row;
+	i = 0;
+	*matrix = n * n;
+	while (*matrix[i] < n)
+	{
+		*matrix[i] = 0;
+		i++;
+	}
+	handle_chess(**matrix, col, row, n);
+}
+
+int ft_ten_queens_puzzle(void)
+{
+	int **matrix;
+	int n;
+	int *arr;
+	int i;
+	i = 0;
+	chess_board(**matrix, n);
+	while (*matrix[i] < n)
+	{
+		if (*matrix[i] == 1)
+			arr++;
+		i++;
+	}
+	return (arr);
 }
